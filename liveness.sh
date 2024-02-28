215bc621aba4bce537a434bef92126dd5b19c955
Stephen Eddy <stephen.eddy@gigabytesol.com>
2024-02-28 13:22:45 +0000

    New simpler liveness probe

--- liveness.sh
+++ liveness.sh
@@ -10,29 +10,18 @@ if [ ! -f "$LOG_FILE" ]; then
     exit 1
 fi
 
-# Extract the last log entry's timestamp and convert it into a format that 'date' understands
-LAST_LOG_ENTRY=$(tail -n 1 "$LOG_FILE")
-LAST_LOG_TIME=$(echo $LAST_LOG_ENTRY | awk -F '[][]' '{print $2}' | sed 's/T/ /; s/Z//')
-
-# Convert the timestamp to Unix epoch time
-LAST_LOG_TIME=$(date -d "$LAST_LOG_TIME" +%s 2>/dev/null)
-echo "Last Log Time: $LAST_LOG_TIME"
-
-# Check if the last log time could be determined
-if [ -z "$LAST_LOG_TIME" ]; then
-    echo "Could not determine the time of the last log entry."
-    exit 1
-fi
+# Get the last modification time of the log file in Unix epoch time
+LAST_MOD_TIME=$(stat -c %Y "$LOG_FILE")
 
 # Get the current time
 CURRENT_TIME=$(date +%s)
 
 # Calculate the time difference
-TIME_DIFF=$(( CURRENT_TIME - LAST_LOG_TIME ))
+TIME_DIFF=$(( CURRENT_TIME - LAST_MOD_TIME ))
 
 # Check if the last log entry is older than the timeout
 if [ $TIME_DIFF -gt $TIMEOUT ]; then
-    echo "The application has not logged anything in the last 5 minutes."
+    echo "The application has not logged anything in the last 10 minutes."
     exit 1
 else
     echo "The application is logging as expected."
